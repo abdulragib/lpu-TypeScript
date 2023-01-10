b2b0bd06f20692bba3807ec00c9764f808576247
Abdul Ragib <abdulragib36@gmail.com>
2023-01-10 12:00:00 +0530

    initial commit

---
+++ Control_Statement_part1.ts
@@ -0,0 +1,20 @@
+function displayFoodInfo(calories, fatGrams) {
+  const caloriesFromFat = fatGrams * 9;
+  if(caloriesFromFat < calories)
+  {
+     console.log(`This food has ${calories} calories greater ${caloriesFromFat} calories from fat.`)
+  }
+  else{
+       const fatPercentage = (caloriesFromFat / calories) * 100;
+       console.log(`This food has ${calories} calories and ${fatPercentage}% of its calories come from fat.`);
+      if (fatPercentage < 30) {
+        console.log("This food is low in fat.");
+      }
+       else{
+           console.log("This food is high in fat.");
+       }
+  }
+}
+
+
+displayFoodInfo(100,10)
