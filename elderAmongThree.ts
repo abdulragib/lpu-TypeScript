71b876b08b505ea6423217d9d9743b43027ee1bb
Abdul Ragib <abdulragib36@gmail.com>
2023-01-23 19:56:21 +0530

    Formated

--- elderAmongThree.ts
+++ elderAmongThree.ts
@@ -1,38 +1,65 @@
-//Isha has three brothers Harsh, Prince and Amit. 
+//Isha has three brothers Harsh, Prince and Amit.
 //Write a program using class object and method to find who is elder brother of Isha
 
-class compare{
-    constructor(public harshAge:number,public ishaAge:number,public princeAge:number,public amitAge:number){
-        this.harshAge=harshAge;
-        this.ishaAge=ishaAge;
-        this.princeAge=princeAge;
-        this.amitAge=amitAge;
-    }
+class compare {
+  constructor(
+    public harshAge: number,
+    public ishaAge: number,
+    public princeAge: number,
+    public amitAge: number
+  ) {
+    this.harshAge = harshAge;
+    this.ishaAge = ishaAge;
+    this.princeAge = princeAge;
+    this.amitAge = amitAge;
+  }
 
-    getter(){
-        console.log("Amit Age:",this.harshAge,
-        "Isha Age",this.ishaAge,
-        "Prince Age:",this.princeAge,
-        "Amit Age:",this.amitAge);
+  getter() {
+    console.log(
+      "Amit Age:",
+      this.harshAge,
+      "Isha Age",
+      this.ishaAge,
+      "Prince Age:",
+      this.princeAge,
+      "Amit Age:",
+      this.amitAge
+    );
+  }
+  elderAmongThree() {
+    if (
+      this.harshAge > this.ishaAge &&
+      this.harshAge > this.princeAge &&
+      this.harshAge > this.amitAge
+    ) {
+      console.log("Harsh is elder brother of Isha");
     }
-    elderAmongThree(){
-        if(this.harshAge > this.ishaAge && this.harshAge > this.princeAge && this.harshAge > this.amitAge){
-            console.log("Harsh is elder brother of Isha");
-        }
-         if(this.ishaAge < this.harshAge && this.ishaAge > this.princeAge && this.ishaAge > this.amitAge){
-            console.log("Isha is younger tha harsh and elder than Pince and Amit");
-        }
-         if(this.princeAge < this.harshAge && this.princeAge < this.ishaAge && this.princeAge>this.amitAge)
-        {
-            console.log("Prince is younger tha harsh and isha and elder elder than Amit");
-        }
-        if(this.amitAge < this.harshAge && this.amitAge < this.ishaAge && this.amitAge<this.princeAge ){
-            console.log("Amit is younger tha harsh, isha and  Amit");
-        }
+    if (
+      this.ishaAge < this.harshAge &&
+      this.ishaAge > this.princeAge &&
+      this.ishaAge > this.amitAge
+    ) {
+      console.log("Isha is younger tha harsh and elder than Pince and Amit");
     }
+    if (
+      this.princeAge < this.harshAge &&
+      this.princeAge < this.ishaAge &&
+      this.princeAge > this.amitAge
+    ) {
+      console.log(
+        "Prince is younger tha harsh and isha and elder elder than Amit"
+      );
+    }
+    if (
+      this.amitAge < this.harshAge &&
+      this.amitAge < this.ishaAge &&
+      this.amitAge < this.princeAge
+    ) {
+      console.log("Amit is younger tha harsh, isha and  Amit");
+    }
+  }
 }
 
-let obj = new compare(50,30,20,15);
-obj.getter()
-obj.elderAmongThree()
-
+let obj = new compare(50, 30, 20, 15);
+obj.getter();
+obj.elderAmongThree();
